95e01dde15e9f4d136c9d7f79ab6a2c9b293f826
Harry Ross <hazross@hotmail.com>
2024-05-05 14:09:30 +1000

    added visual feedback for selected conversation in sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 import { type Conversation } from "@/types/chat";
 import { useState } from "react";
 import { DarkModeButton } from "./DarkModeButton";
+import { useSelectedLayoutSegment } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 interface SidebarProps {
   conversations: Conversation[];
@@ -13,6 +15,12 @@ interface SidebarProps {
 
 export const Sidebar = ({ conversations }: SidebarProps): JSX.Element => {
   const [open, setOpen] = useState(true);
+
+  const segment = useSelectedLayoutSegment();
+  let currentChatId: number | undefined;
+  if (segment && parseInt(segment)) {
+    currentChatId = parseInt(segment);
+  }
 
   return (
     <div className="flex flex-col bg-secondary text-secondary-foreground">
@@ -37,7 +45,12 @@ export const Sidebar = ({ conversations }: SidebarProps): JSX.Element => {
               href={`/chat/${conversation.conversation_id}`}
               key={conversation.title + index}
             >
-              <div className="mx-1 my-2 rounded-lg p-2 hover:bg-neutral-700">
+              <div
+                className={cn("mx-1 my-2 rounded-lg p-2 hover:bg-neutral-700", {
+                  "bg-neutral-700":
+                    currentChatId === conversation.conversation_id,
+                })}
+              >
                 {conversation.title}
               </div>
             </Link>
